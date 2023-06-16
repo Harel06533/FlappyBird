32cc631e0b48530b887e2fce398a79f654bd8754
Ethernoss <jorgearriaga1102@gmail.com>
2023-06-15 20:39:56 -0600

    chupas

--- src/app/App.java
+++ src/app/App.java
@@ -4,7 +4,7 @@ package app;
  * Implementación de un juego tipo Flappy Bird (Es un proyecto universitario) 
  * Esta implementación está basada enormemente en el código de <a href="https://github.com/kingyuluk/FlappyBird/tree/3df72aeaf881c90be38e716e3193df1e9323371e"><strong>kingyuluk</strong></a> en GitHub
  * @author Harel Alejandro Olguín Gaytán
- * @author Jorge
+ * @author Jorge Francisco Arriaga Escamilla
  * <h3>Inspiración para el programa</h3>
  * @see <a href="https://github.com/kingyuluk/FlappyBird/tree/3df72aeaf881c90be38e716e3193df1e9323371e">kingyuluk/GitHub</a>
  * @see <a href = "https://github.com/TheCherno/Flappy/tree/master">The Cherno</a>
