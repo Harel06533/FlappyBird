184edc8d661893c4bcf083a2055462d3a802ac0e
Ethernoss <jorgearriaga1102@gmail.com>
2023-06-23 21:24:26 -0600

    Class Background and Class Bird implemented

--- src/components/Bird.java
+++ src/components/Bird.java
@@ -11,24 +11,29 @@ public class Bird {
     private static int birdPosX;
     private static int birdPosY;
 
+    // Constructor privado
     private Bird() {
         bird0 = new ImageIcon(birdpath);
         birdPosX = 0;
         birdPosY = 0;
     }
 
+    // Establece posición del pajaro en X
     public void setBirdPosX(int birdPosX) {
         Bird.birdPosX = birdPosX;
     }
 
+    // Establece posición del pajaro en Y
     public void setBirdPosY(int birdPosY) {
         Bird.birdPosY = birdPosY;
     }
 
+    //Dibuja el pajaro
     public void drawBird(Graphics g) {
         g.drawImage(bird0.getImage(), birdPosX, birdPosY, null);
     }
 
+    // Obtiene 
     public static Bird getBird() {
         if (instance == null) {
             instance = new Bird();
